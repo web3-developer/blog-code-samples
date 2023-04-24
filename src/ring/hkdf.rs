5f33fe5d622b75f683edba46f816175488bd93d5
Web3 Developer <web3developer@protonmail.com>
2023-04-24 13:14:00 +0800

    Refactored hkdf example code.

--- src/ring/hkdf.rs
+++ src/ring/hkdf.rs
@@ -5,54 +5,50 @@ mod tests {
     //use super::*;
 
     use ring::digest::SHA256_OUTPUT_LEN;
-    use ring::hkdf::{Algorithm, KeyType};
-    use ring::hkdf::Okm;
-    use ring::hkdf::Prk;
-    use ring::hkdf::Salt;
-    //use ring::hkdf::KeyType;
     //use ring::hkdf::HKDF_SHA1_FOR_LEGACY_USE_ONLY;
     use ring::hkdf::HKDF_SHA256;
+    //use ring::hkdf::HKDF_SHA384;
     //use ring::hkdf::HKDF_SHA512;
+    use ring::hkdf::KeyType;
+    use ring::hkdf::Algorithm;
+    use ring::hkdf::Salt;
+    use ring::hkdf::Prk;
+    use ring::hkdf::Okm;
 
-    // impl KeyType for Algorithm {
-    //     fn len(&self) -> usize {
-    //         self.0.digest_algorithm().output_len
-    //     }
-    // }
 
     #[test]
     fn run() {
         let input_key_material = b"secret key";
         println!("Input key material: {}", hex::encode(input_key_material));
 
-        // salt
+        // Constructs a new Salt with the given value based on the given digest algorithm
         let salt = Salt::new(HKDF_SHA256, b"salt bytes");
 
-        // Constructs a new Salt with the given value based on the given digest algorithm.
-        // Constructing a Salt is relatively expensive so it is good to reuse a Salt object instead of re-constructing Salts with the same value.
-        // prk
+        // The HKDF-Extract operation
         let pseudo_rand_key: Prk = salt.extract(input_key_material);
 
-        // The HKDF-Expand  operation.
-        // Fails if (and only if) len is too large.
-        // okm
-        let context_data = &["context info".as_bytes()];
+        // The HKDF-Expand operation
+        let context_data = &["context one".as_bytes()];
         let output_key_material: Okm<Algorithm> = pseudo_rand_key.expand(context_data, HKDF_SHA256).unwrap();
 
         let mut result = [0u8; SHA256_OUTPUT_LEN];
         output_key_material.fill(&mut result).unwrap();
         println!("Derived output key material: {}", hex::encode(result));
 
-        // multiple calls to expand produce the same output
-        // let output_key_material: Okm<Algorithm> = pseudo_rand_key.expand(context_data, HKDF_SHA256).unwrap();
-        //
-        // let mut result = [0u8; SHA256_OUTPUT_LEN];
-        // output_key_material.fill(&mut result).unwrap();
-        // println!("Derived output key material: {}", hex::encode(result));
+        // second call with different context data produces a different output
+        let context_data = &["context two".as_bytes()];
+        let output_key_material: Okm<Algorithm> = pseudo_rand_key.expand(context_data, HKDF_SHA256).unwrap();
+
+        let mut result = [0u8; SHA256_OUTPUT_LEN];
+        output_key_material.fill(&mut result).unwrap();
+        println!("Derived output key material: {}", hex::encode(result));
     }
 
     #[test]
     fn run_expand_larger_len() {
+        const NUM_OF_KEYS: usize = 3;
+        const OUTPUT_KEY_SIZE: usize = NUM_OF_KEYS * SHA256_OUTPUT_LEN;
+
         struct MyKeyType(usize);
 
         impl KeyType for MyKeyType {
@@ -61,35 +57,26 @@ mod tests {
             }
         }
 
-        let key_size = MyKeyType(SHA256_OUTPUT_LEN * 255);
-        //let key_size = MyKeyType(SHA256_OUTPUT_LEN * 256); // fails
-
         let input_key_material = b"secret key";
         println!("Input key material: {}", hex::encode(input_key_material));
 
-        // salt
+        // Constructs a new Salt with the given value based on the given digest algorithm
         let salt = Salt::new(HKDF_SHA256, b"salt bytes");
 
-        // Constructs a new Salt with the given value based on the given digest algorithm.
-        // Constructing a Salt is relatively expensive so it is good to reuse a Salt object instead of re-constructing Salts with the same value.
-        // prk
+        // The HKDF-Extract operation
         let pseudo_rand_key: Prk = salt.extract(input_key_material);
 
-        // The HKDF-Expand  operation.
-        // Fails if (and only if) len is too large.
-        // okm
-        let context_data = &["context info".as_bytes()];
-        let output_key_material: Okm<MyKeyType> = pseudo_rand_key.expand(context_data, key_size).unwrap();
+        // The HKDF-Expand operation
+        let context_data = &["context one".as_bytes()];
+        let output_key_material: Okm<MyKeyType> = pseudo_rand_key.expand(context_data, MyKeyType(OUTPUT_KEY_SIZE)).unwrap();
 
-        let mut result = [0u8; SHA256_OUTPUT_LEN * 255];
+        let mut result = [0u8; OUTPUT_KEY_SIZE];
         output_key_material.fill(&mut result).unwrap();
-        println!("Derived output key material: {}", hex::encode(result));
-
-        // cannot call fill multiple times
-        // let mut result = [0u8; SHA256_OUTPUT_LEN * 2];
-        // output_key_material.fill(&mut result).unwrap();
-        // println!("Derived output key material: {}", hex::encode(result));
+        println!("Derived output key material: {}", hex::encode(&result[0..32]));
+        println!("Derived output key material: {}", hex::encode(&result[32..64]));
+        println!("Derived output key material: {}", hex::encode(&result[64..96]));
     }
+
 }
 
 
