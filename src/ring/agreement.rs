eb7d7ab43a7cdad419d4dc2e6b294b6a03f1831f
Web3 Developer <51288821+web3-developer@users.noreply.github.com>
2023-05-14 22:34:18 +0800

    Added sample code for agreement module.

--- src/ring/agreement.rs
+++ src/ring/agreement.rs
@@ -4,11 +4,56 @@
 mod tests {
     //use super::*;
 
+    use ring::error::Unspecified;
+    use ring::rand::SystemRandom;
+    use ring::agreement::Algorithm;
+    // use ring::agreement::ECDH_P256;
+    // use ring::agreement::ECDH_P384;
+    use ring::agreement::X25519;
+    use ring::agreement::EphemeralPrivateKey;
+    use ring::agreement::PublicKey;
+    use ring::agreement::UnparsedPublicKey;
+    use ring::agreement::agree_ephemeral;
+
     #[test]
-    fn run() {
+    fn run() -> Result<(), Unspecified> {
+        // Use a rand::SystemRandom as the source of entropy
+        let rng = SystemRandom::new();
 
+        // Select a key agreement algorithm. All agreement algorithms follow the same flow
+        let alg: &Algorithm = &X25519;
+
+        // Generate a private key and public key
+        let my_private_key: EphemeralPrivateKey = EphemeralPrivateKey::generate(alg, &rng)?;
+        let my_public_key: PublicKey = my_private_key.compute_public_key()?;
+        // The EphemeralPrivateKey doesn't allow us to directly access the private key as designed
+        println!("my_public_key = {}", hex::encode(my_public_key.as_ref()));
+
+        // Send our public key to the peer here
+
+        // Simulate receiving a public key from the peer
+        let peer_public_key: PublicKey = {
+            let peer_private_key = EphemeralPrivateKey::generate(alg, &rng)?;
+            peer_private_key.compute_public_key()?
+        };
+        println!("peer_public_key = {}", hex::encode(peer_public_key.as_ref()));
+
+        // The peer public key needs to be parsed before use so wrap it creating as an instance of UnparsedPublicKey
+        let peer_public_key = UnparsedPublicKey::new(alg, peer_public_key);
+
+        // run ECDH to agree on a shared secret
+        agree_ephemeral(my_private_key, &peer_public_key, Unspecified, // error to return on failure
+            |shared_secret: &[u8]| { // the result of the key agreement is passed to this lambda
+                println!("shared_secret = {}", hex::encode(shared_secret.as_ref())); // don't print this in production
+
+                // As recommended in RFC 7748 we should apply a KDF on the key material here before using in a real application
+
+                // We can return the derived key from the kdf here, otherwise we just return () if the key isn't needed outside this scope
+                Ok(())
+            },
+        )
     }
-
+    
 }
 
 
